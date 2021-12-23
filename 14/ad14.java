adb8ff8b18882c758566aba65375bfc6433f62b2
totallyrin <lucy.woloszczuk@gmail.com>
2021-12-22 21:56:40 -0500

    day 14

--- 14/ad14.java
+++ 14/ad14.java
@@ -1,14 +1,92 @@
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.Arrays;
+import java.util.HashSet;
 import java.util.Scanner;
+import java.util.Set;
 
 public class ad14 {
-    public static int part1(String[] input) {
-        return 0;
+    public static long part1(String[] input) {
+        // read input
+        String template = input[0];
+        String[][] pairs = new String[input.length - 2][2];
+        for (int i = 0; i < pairs.length; i++) {
+            String[] temp = input[i + 2].split(" -> ");
+            pairs[i] = temp;
+        }
+        for (int step = 0; step < 10; step++) {
+            StringBuilder result = new StringBuilder();
+            for (int letter = 0; letter < template.length() - 1; letter++) {
+                String tempPair = template.substring(letter, letter + 2);
+                for (String[] pair : pairs) {
+                    if (pair[0].equals(tempPair)) {
+                        result.append(tempPair.charAt(0));
+                        result.append(pair[1]);
+                        if (letter == template.length() - 2)
+                            result.append(tempPair.charAt(1));
+                        break;
+                    }
+                }
+            }
+            template = result.toString();
+        }
+        int[] count = new int[26];
+        for (char c : template.toCharArray())
+            count[c - 65]++;
+        int max = 0, min = Integer.MAX_VALUE;
+        for (int i : count) {
+            if (i == 0)
+                assert true;
+            else if (i > max)
+                max = i;
+            else if (i < min)
+                min = i;
+        }
+        return max - min;
     }
 
     public static int part2(String[] input) {
-        return 0;
+        // read input
+        String template = input[0];
+        String[][] pairs = new String[input.length - 2][2];
+        for (int i = 0; i < pairs.length; i++) {
+            String[] temp = input[i + 2].split(" -> ");
+            pairs[i] = temp;
+        }
+        // read template into pairs
+        Set<String> ps = new HashSet<>();
+        Set<String> elems = new HashSet<>();
+
+
+        for (int step = 0; step < 10; step++) {
+            StringBuilder result = new StringBuilder();
+            for (int letter = 0; letter < template.length() - 1; letter++) {
+                String tempPair = template.substring(letter, letter + 2);
+                for (String[] pair : pairs) {
+                    if (pair[0].equals(tempPair)) {
+                        result.append(tempPair.charAt(0));
+                        result.append(pair[1]);
+                        if (letter == template.length() - 2)
+                            result.append(tempPair.charAt(1));
+                        break;
+                    }
+                }
+            }
+            template = result.toString();
+        }
+        int[] count = new int[26];
+        for (char c : template.toCharArray())
+            count[c - 65]++;
+        int max = 0, min = Integer.MAX_VALUE;
+        for (int i : count) {
+            if (i == 0)
+                assert true;
+            else if (i > max)
+                max = i;
+            else if (i < min)
+                min = i;
+        }
+        return max - min;
     }
 
     public static void main(String[] args) throws FileNotFoundException {
