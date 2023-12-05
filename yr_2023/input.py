66707a95a76f1e37bc721cf5adc7788308eca2e6
Lucy W <lucy.woloszczuk@gmail.com>
2023-12-05 11:34:28 -0500

    no longer reads empty lines

--- yr_2023/input.py
+++ yr_2023/input.py
@@ -5,5 +5,7 @@ def read_lines(f: str) -> List[str]:
     file = open(f, "r")
     lines = []
     for line in file:
+        if line == "\n":
+            continue
         lines.append(line.strip())
     return lines
